d14231fdf143868f9c664a4e220afe4d92cc1838
Kevin Wang <ltwoggbuty2mh@gmail.com>
2023-04-07 16:30:40 +0800

    Initial commit

---
+++ src/components/TestList.vue
@@ -0,0 +1,8 @@
+<template> this is the test
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,14 @@
+<template>
+  文章列表
+  <img :src="gege" class="gege" />
+</template>
+
+<script setup lang="ts">
+import gege from "../assets/gege.png";
+</script>
+
+<style scoped>
+.gege {
+  width: 20px;
+}
+</style>
